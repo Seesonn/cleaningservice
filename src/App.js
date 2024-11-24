343138e826f7131f5a0a8f50e6301c440524f27c
Sisan Bhattarai <125193929+Seesonn@users.noreply.github.com>
2024-11-24 11:46:51 +0545

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import CleaningService from "./angst"
+import CleaningService from "./assests/assets/components/CleaningService"
+import Gallery from "./assests/assets/components/gallery"
 function App() {
   return (
     <div className="App">
   <CleaningService/>
+    <Gallery/>
     </div>
   );
 }
